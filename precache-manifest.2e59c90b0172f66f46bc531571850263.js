ad80981f29b6deb1eb4446c06257a024ee7e502d
Shubham Dhage <shubhamdhage930@gmail.com>
2019-04-28 07:57:31 +0530

    Updates

---
+++ precache-manifest.2e59c90b0172f66f46bc531571850263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280a14a4dff3735fc00f9ddb43123ae9",
+    "url": "/blogpost-redux/index.html"
+  },
+  {
+    "revision": "abfffc0aac3939f85981",
+    "url": "/blogpost-redux/static/css/main.a4159582.chunk.css"
+  },
+  {
+    "revision": "bdeafae70c35151bc739",
+    "url": "/blogpost-redux/static/js/2.e0c8742e.chunk.js"
+  },
+  {
+    "revision": "abfffc0aac3939f85981",
+    "url": "/blogpost-redux/static/js/main.30407b35.chunk.js"
+  },
+  {
+    "revision": "1351033225e95be71f01",
+    "url": "/blogpost-redux/static/js/runtime~main.98ef5386.js"
+  }
+]);
